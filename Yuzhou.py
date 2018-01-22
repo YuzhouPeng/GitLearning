9b82d17e71aa19f9877dae7897a0e6df6c7c1778
macjiong <760783896@qq.com>
2018-01-22 17:21:52 +0000

    revised function

--- Yuzhou.py
+++ Yuzhou.py
@@ -7,8 +7,19 @@ def find_substring(string, sub):
         :type sub: str
         :rtype: int
         """
-        str = string
-        initialpos = str.find(sub)
-        return initialpos
+        # use flag to identify the substring position
+        flag = 0
+        for i in range(0,len(string)):
+            if string[i] == sub[0]:
+                for j in range(0,len(sub)):
+                    if j <= i and string[i+j] == sub[j]:
+                        flag = 1
+                    else:
+                        flag = 0
+                if flag == 1:
+                    # if find the result, return the position
+                    return i
+                # else, return none
+        return None
 
 
